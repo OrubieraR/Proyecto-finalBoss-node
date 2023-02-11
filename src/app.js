3c688cd624ada3741b2148d4055ae18770d36f8c
Cristina Kukhar <criskukhar@hotmail.com>
2023-02-11 20:27:53 +0100

    login done.initDB modified.CORS

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const LoginController = require("../controller/loginController");
+const cors = require("cors")
 
 //Configuraciones
+app.use(cors())
 app.set("port", process.env.PORT || 3000);
 app.set("json spaces", 2);
+
+require('./lib/MongooseConnection');
 
 //Middleware
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const loginController = new LoginController();
+
+// API Route
+app.use('/api/login',   loginController.post);
 
 //Nuestro primer WS Get
 app.get("/", (req, res) => {
