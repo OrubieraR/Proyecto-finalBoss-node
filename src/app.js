75338a140cfbedadf7ccbefcb552213f44b8de64
David Monroy <david2310.dmc@gmail.com>
2023-02-11 22:06:34 +0000

    Problems with merge

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const register = require("./routes/register")
 const LoginController = require("../controller/loginController");
 const cors = require("cors")
+require("./lib/MongooseConnection");
 
+const adverts = require("../initialAdverts")
+const users = require("../initialUser")
 //Configuraciones
 app.use(cors())
 app.set("port", process.env.PORT || 3000);
@@ -13,20 +17,20 @@ require('./lib/MongooseConnection');
 
 //Middleware
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(express.json());
-
 const loginController = new LoginController();
 
 // API Route
 app.use('/api/login',   loginController.post);
-
 //Nuestro primer WS Get
 app.get("/", (req, res) => {
-  res.json({
-    Title: "Hola mundo",
-  });
+  res.send("Welcome to the game online shop API...");
 });
+
+app.get("/adverts", (req,res)=>{
+  res.send(adverts)
+})
 
 //Iniciando el servidor
 app.listen(app.get("port"), () => {
---
+++ src/routes/register.js
@@ -0,0 +1,38 @@
+const bcrypt = require("bcrypt");
+const express = require("express");
+const Joi = require("joi");
+const User = require("../models/User");
+const genAuthToken = require("../utils/genAuthToken");
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(3).max(200).required().email(),
+    password: Joi.string().min(3).max(1024).required(),
+  });
+  const { error } = schema.validate(req.body);
+
+  if (error) return res.status(400).send(error.details[0]);
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already exist...");
+
+  user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  //hasheamos la pass
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  user = await user.save();
+
+  const token = genAuthToken(user);
+
+  res.send(token);
+});
+
+module.exports = router;
