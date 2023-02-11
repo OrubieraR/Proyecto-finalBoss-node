1f4d1b0ac0343fdee8ebd13a97c9bf1bff516596
David Monroy <david2310.dmc@gmail.com>
2023-02-11 22:08:10 +0000

    uniendo ramas

--- src/app.js
+++ src/app.js
@@ -1,32 +1,33 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const LoginController = require("../controller/loginController");
 const cors = require("cors")
+const register = require("./routes/register")
+require("./lib/MongooseConnection");
 
+const adverts = require("../initialAdverts")
+const users = require("../initialUser")
 //Configuraciones
-app.use(cors())
 app.set("port", process.env.PORT || 3000);
 app.set("json spaces", 2);
 
-require('./lib/MongooseConnection');
-
 //Middleware
 app.use(morgan("dev"));
-app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(express.json());
 
-const loginController = new LoginController();
+app.use("/api/register",register)
 
-// API Route
-app.use('/api/login',   loginController.post);
+app.use(express.urlencoded({ extended: false }));
 
 //Nuestro primer WS Get
 app.get("/", (req, res) => {
-  res.json({
-    Title: "Hola mundo",
-  });
+  res.send("Welcome to the game online shop API...");
 });
+
+app.get("/adverts", (req,res)=>{
+  res.send(adverts)
+})
 
 //Iniciando el servidor
 app.listen(app.get("port"), () => {
