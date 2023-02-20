28acdc440a9e95d70d4408ca6fd9e5103e8d7eeb
Cristina Kukhar <criskukhar@hotmail.com>
2023-02-20 14:22:54 +0100

    login sending user data

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,6 +1,6 @@
 "use strict";
-const jwt = require("jsonwebtoken");
 const { User } = require("../src/models");
+const genAuthToken = require("../src/utils/genAuthToken");
 
 class LoginController {
   async post(req, res, next) {
@@ -9,17 +9,17 @@ class LoginController {
 
       const user = await User.findOne({ name });
 
-      if (!user || !(await user.checkPwd(password))) {
-        const status = 401;
-        const message = "Wrong username or password";
-        return res.status(status).json({ message });
+      if (user && (await user.checkPwd(password))) {
+        res.json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          userToken: genAuthToken(user._id),
+        })
+      } else {
+        res.status(401)
+        throw new Error('Wrong name or password')
       }
-
-      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_KEY, {
-        expiresIn: "2d",
-      });
-
-      return res.status(200).json({ token });
     } catch (err) {
       next(err);
     }
